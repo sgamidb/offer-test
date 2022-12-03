406402f08a17285be9070329c01c0802fdc0961a
abdellah <abdellah.essofyany@gmail.com>
2022-12-03 11:48:57 +0100

    refacto filter data

--- src/filter-data.js
+++ src/filter-data.js
@@ -1,24 +1,32 @@
 const EMPTY_ARRAY = [];
 
 function filterData(filter, data = [],) {
+    return reduceCountries(data, filter)
+}
+
+function reduceCountries(data, filter) {
     return data.reduce((accumulator, currentValue) => {
-        const reducedPeople = currentValue.people.reduce((accumulator, currentValue) => {
-            const filteredAnimals = currentValue.animals.filter(animal => animal.name.includes(filter));
-            if (filteredAnimals.length) {
-                return accumulator.concat({
-                    name: currentValue.name,
-                    animals: filteredAnimals
-                });
-            }
-            return accumulator
-        }, EMPTY_ARRAY);
+        const reducedPeople = reducePeople(currentValue, filter);
 
         return reducedPeople.length ? accumulator.concat({
             name: currentValue.name,
             people: reducedPeople
         }) : accumulator;
 
-    }, EMPTY_ARRAY)
+    }, EMPTY_ARRAY);
+}
+
+function reducePeople(currentValue, filter) {
+    return currentValue.people.reduce((accumulator, currentValue) => {
+        const filteredAnimals = currentValue.animals.filter(animal => animal.name.includes(filter));
+        if (filteredAnimals.length) {
+            return accumulator.concat({
+                name: currentValue.name,
+                animals: filteredAnimals
+            });
+        }
+        return accumulator
+    }, EMPTY_ARRAY);
 }
 
 exports.filterData = filterData
